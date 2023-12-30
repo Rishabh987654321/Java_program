e01b2df08c5601b89b651bb270af04bb79205f67
Rishabh kumar <rishabhkumar90063@gmail.com>
2023-12-30 19:08:17 +0530

    Add files via upload

---
+++ recursion/countNumberOfDigits.java
@@ -0,0 +1,21 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class countNumberOfDigits {
+    static int COD(int n) {
+
+        if (n >= 0 && n <= 9) {
+            return 1;
+        }
+        return COD(n / 10) + 1;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(COD(n));
+    }
+
+}
